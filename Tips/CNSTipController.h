9999ca2d9aaaad0fbcad084ad1f779e21b54f453
Ezra Black <dev.ezrablack@gmail.com>
2020-06-10 01:27:57 -0500

    implemented our tip controller logic with access control

--- Tips/CNSTipController.h
+++ Tips/CNSTipController.h
@@ -7,10 +7,15 @@
 //
 
 #import <Foundation/Foundation.h>
+@class CNSTip; //lets it pre know about the class tip. helps it build fast by being outside the compiling interface. and can reference other classes recursively.
 
 NS_ASSUME_NONNULL_BEGIN
-
+//public all of this is public
 @interface CNSTipController : NSObject
+
+@property (nonatomic, readonly) NSArray<CNSTip *> *tips;
+
+- (void)addTip:(CNSTip *)tips;
 
 @end
 
--- Tips/CNSTipController.m
+++ Tips/CNSTipController.m
@@ -7,7 +7,38 @@
 //
 
 #import "CNSTipController.h"
+#import "CNSTip.h"
+//class extension
+@interface CNSTipController () {
+    //private instance variables that are optional
+}
+//private properties
+@property (nonatomic, readwrite) NSMutableArray<CNSTip *> *internalTips;
+
+//private outlets
+
+//private methods
+
+
+@end
 
 @implementation CNSTipController
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _internalTips = [[NSMutableArray alloc]init];
+    }
+    return self;
+}
+
+- (NSArray<CNSTip *> *)tips
+{
+    return [_internalTips copy];
+}
+
+- (void)addTip:(CNSTip *)tip {
+    [self.internalTips addObject:tip];
+}
 
 @end
